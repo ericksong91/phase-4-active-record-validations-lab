749094ea77bf314989af9ecc33c5ace77f4e0d64
Eric Song <ericksong91@gmail.com>
2023-02-13 17:43:54 -0500

    Done with assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,30 @@
+
+
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :clickbait?
+
+    # pattern = [/Won't Believe/i, /True Facts/i]
+
+    # def clickbait?
+    #     if pattern.none? {|p| p.match title}
+    #         errors.add(:title, "Cannot be clickbait")
+    #     end
+    # end
+
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i
+      ]
+    
+      def clickbait?
+        byebug
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+      end
+
 end
